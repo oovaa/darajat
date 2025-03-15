aeedc1d268a7a1e392b0ecf401886dfb69a9b965
Amar Yasir <3marofficial249@gmail.com>
2025-03-15 22:55:43 +0200

    myLearning Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { HomePage, Onboarding } from './Container';
+import { HomePage, MyLearning, Onboarding } from './Container';
 
 const App = () => {
   return (
@@ -8,6 +8,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='onboarding' element={<Onboarding />} />
+          <Route path='MyLearning' element={<MyLearning/>} />
         </Routes>
       </BrowserRouter>
     </>
