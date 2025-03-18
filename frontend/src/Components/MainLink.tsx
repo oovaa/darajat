a35fb7077d04dd2c86416f1e28731bba725ec0e7
A1iHassan <ali012wkout@gmail.com>
2025-03-18 19:27:52 +0200

    added to the learning plan page

--- frontend/src/Components/MainLink.tsx
+++ frontend/src/Components/MainLink.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 interface MainLinkProps {
-    title: string;
-    route: string;
-    className?: string;
+  title: string;
+  route: string;
+  className?: string;
 }
 
 const MainLink: React.FC<MainLinkProps> = ({ title, className, route }) => {
-    return (
-        <Link to={route} className={`inline-block bg-orange-400 text-white font-bold border-none outline-none cursor-pointer rounded-3xl px-8 py-2.5 my-2 hover:-translate-y-1 transition-all ${className}`}>{title}</Link>
-    )
-}
+  return (
+    <Link
+      to={route}
+      className={`inline-block bg-orange-400 text-white font-bold border-none outline-none cursor-pointer rounded-3xl px-8 py-2.5 my-2 hover:-translate-y-1 transition-all ${className}`}
+    >
+      {title}
+    </Link>
+  );
+};
 
 export default MainLink;
