b8d1ff372f4189c963ed7297d3aa41b8707f2958
Amar Yasir <3marofficial249@gmail.com>
2025-03-18 21:26:12 +0200

    .

--- frontend/src/Components/MainLink.tsx
+++ frontend/src/Components/MainLink.tsx
@@ -9,8 +9,13 @@ interface MainLinkProps {
 
 const MainLink: React.FC<MainLinkProps> = ({ title, className, route }) => {
     return (
-        <Link to={route}  className={`inline-block bg-orange-400 text-white font-bold border-none outline-none cursor-pointer rounded-3xl px-8 py-2.5 my-2 hover:-translate-y-1 transition-all ${className}`}>{title}</Link>
-    )
-}
+        <Link
+          to={route}
+          className={`inline-block bg-orange-400 text-white font-bold border-none outline-none cursor-pointer rounded-3xl px-8 py-2.5 my-2 hover:-translate-y-1 transition-all ${className}`}
+        >
+          {title}
+        </Link>
+      );
+    };
 
 export default MainLink;
