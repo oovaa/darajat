ce7e82a44cf67988407c631a27e64a53f23242de
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2025-03-20 16:38:59 +0400

    A util function to retrieve a list of subjects to cover within a week

---
+++ frontend/src/Utils/generateContent.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const generateContent = (data: any) => {
+
+
+    const week = Object.values(data.plan[0].weeks[0].daily_focus)
+    console.log(week)
+    const flattenedSubjects = week
+    .flatMap((day: any) => Object.values(day.subjects).flat());
+    console.log(flattenedSubjects);
+}
